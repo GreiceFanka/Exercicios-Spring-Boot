0653fdfd450ec86caf13554020cb29058707e452
GreiceFanka <greicefcardoso@gmail.com>
2023-05-11 12:48:11 +0200

    Interface para injeçao de dependencias

---
+++ src/main/java/com/example/exerciciossb/models/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.example.exerciciossb.models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
+	
+}
