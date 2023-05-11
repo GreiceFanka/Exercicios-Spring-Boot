f8885ec23419e12b8c82a7bc27a7cf8d6f65c052
GreiceFanka <greicefcardoso@gmail.com>
2023-05-11 12:50:01 +0200

    Controller para inserçao de novos produtos

---
+++ src/main/java/com/example/exerciciossb/controller/ProdutoController.java
@@ -0,0 +1,24 @@
+package com.example.exerciciossb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.exerciciossb.models.Produto;
+import com.example.exerciciossb.models.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@PostMapping
+	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
+		Produto produto = new Produto(nome);
+		produtoRepository.save(produto);
+		return produto;
+	}
+}
