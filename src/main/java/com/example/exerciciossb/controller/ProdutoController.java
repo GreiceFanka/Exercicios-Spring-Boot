6ede6253f148934c7bb0f95d3ff4af9497478761
GreiceFanka <greicefcardoso@gmail.com>
2023-05-12 11:15:44 +0200

    add metodo para consultar produtos

--- src/main/java/com/example/exerciciossb/controller/ProdutoController.java
+++ src/main/java/com/example/exerciciossb/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.example.exerciciossb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,5 +20,9 @@ public class ProdutoController {
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
+	}
+	@GetMapping
+	public Iterable<Produto> obterProdutos(){
+		return produtoRepository.findAll();
 	}
 }
