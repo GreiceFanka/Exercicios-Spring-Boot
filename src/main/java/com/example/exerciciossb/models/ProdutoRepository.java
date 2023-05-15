2c4576c48109777b8b5c8225fb4bafc1107eb234
GreiceFanka <greicefcardoso@gmail.com>
2023-05-15 13:20:20 +0200

    add repositorio para paginacao/metodo para obter produto por nome

--- src/main/java/com/example/exerciciossb/models/ProdutoRepository.java
+++ src/main/java/com/example/exerciciossb/models/ProdutoRepository.java
@@ -1,7 +1,8 @@
 package com.example.exerciciossb.models;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
-	
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>, CrudRepository<Produto, Integer>{
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 }
